9ad077adb827fb79e18a83e44b963cb4ab79255e
mn0803 <watervalley83@gmail.com>
2025-10-10 16:51:21 +0900

    section12

---
+++ src/components/pages/UserManagement.tsx
@@ -0,0 +1,59 @@
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { memo, useCallback, useEffect } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { useLoginUser } from "../../hooks/useLoginUser";
+import { useSelectUser } from "../../hooks/useSelectUser";
+import { UserCard } from "../organisms/user/UserCard";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
+
+export const UserManagement = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { getUsers, users, loading } = useAllUsers();
+  const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+
+  useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen });
+    },
+    [users, onSelectUser, onOpen]
+  );
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id}>
+              <UserCard
+                id={user.id}
+                imageUrl="https://images.unsplash.com/photo-1570824103090-72cf4906868c?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhdHxlbnwwfDJ8MHx8fDI%3D"
+                userName={user.username}
+                fullName={user.name}
+                onClick={onClickUser}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+      <UserDetailModal
+        isOpen={isOpen}
+        onClose={onClose}
+        user={selectedUser}
+        isAdmin={loginUser?.isAdmin ?? false}
+      />
+    </>
+  );
+});
